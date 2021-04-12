687ee1f78ac372c67af94c7aa3865af530d51ef4
Eetu Karttunen <62899180+eetukarttunen@users.noreply.github.com>
2021-04-12 15:07:36 +0300

    Tehtävät 4.1-4.6, 4.8-4.10, 4.13 @eetukarttunen
    Tehtävät 4.1-4.6, 4.8-4.10, 4.13 @eetukarttunen

---
+++ osa4/blogilista/controllers/blogs.js
@@ -0,0 +1,67 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+  
+  blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  // Tehtävä 4.13.
+  blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  })
+  
+
+  blogsRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id)
+      .then(blog => {
+        if (blog) {
+          response.json(blog.toJSON())
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+  
+  blogsRouter.post('/', (request, response) => {
+    const body = request.body
+    
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    })
+
+    
+  
+    blog.save()
+      .then(savedBlog => {
+        response.json(savedBlog.toJSON())
+      })
+      .catch(error => next(error))
+  })
+  
+ blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+  
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    }
+  
+    Blog.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedBlog => {
+        response.json(updatedBlog.toJSON())
+      })
+      .catch(error => next(error))
+  })
+  
+  module.exports = blogsRouter
